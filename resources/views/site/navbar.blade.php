3ed59eeea2d329b182a828e78a2f30c242c710be
Kandita Digital Integrasi <148229525+kanditadigital@users.noreply.github.com>
2025-10-04 03:19:31 +0700

    Update

--- resources/views/site/navbar.blade.php
+++ resources/views/site/navbar.blade.php
@@ -1,13 +1,12 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Examdita</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ms-auto">
           <a class="nav-link me-3" aria-current="page" href="/">Beranda</a>
-          <a class="nav-link me-3" href="#about">Tentang</a>
           <a class="nav-link me-3" href="#statistic">Statistik</a>
           <a class="nav-link" href="{{ route('live-score') }}">Live Score</a>
         </div>
